1df30c33f675ed3fc734e86feb6fef4b20190d1f
Krishna Raman <kraman@gmail.com>
2012-08-29 09:57:20 -0700

    Fixes for listing standalone/embedded carts (backward compat with legacy api) Fixes for legacy broker (backward compat)
    Starting to add back models and controllers that I had removed earlier

--- stickshift/controller/app/rest_models/rest_cartridge11.rb
+++ stickshift/controller/app/rest_models/rest_cartridge11.rb
@@ -1,7 +1,7 @@
 class RestCartridge11 < StickShift::Model
 
   attr_accessor :type, :name, :version, :license, :license_url, :tags, :website, :suggests, :requires, :conflicts, :provides,
-  :help_topics, :links, :properties, :display_name, :description, :scales_with, :status_messages
+    :help_topics, :links, :properties, :display_name, :description, :scales_with, :status_messages
 
   def initialize(type, cart, app, cinst, url, nolinks=false)
     self.name = cart.name
@@ -16,6 +16,9 @@ class RestCartridge11 < StickShift::Model
     self.suggests = cart.suggests
     self.requires = cart.requires
     self.conflicts = cart.conflicts
+    self.type = "standalone"
+    self.type = "embedded" if cart.categories.include? "embedded"
+
     if app.nil?
       self.provides = cart.features
     else
