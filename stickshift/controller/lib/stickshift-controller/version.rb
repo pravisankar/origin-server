34669ec809d320ba5c53844663f11b07255e6e69
Troy Dawson <tdawson@redhat.com>
2012-09-13 17:19:34 -0500

    changing spec file to start with rubygem-

--- stickshift/controller/lib/stickshift-controller/version.rb
+++ stickshift/controller/lib/stickshift-controller/version.rb
@@ -1,5 +1,5 @@
 module StickShift
-  VERSION = File.open("#{File.dirname(__FILE__)}/../../stickshift-controller.spec"
+  VERSION = File.open("#{File.dirname(__FILE__)}/../../rubygem-stickshift-controller.spec"
                         ).readlines.delete_if{ |x| !x.match(/Version:/)
                         }.first.split(':')[1].strip
 end
